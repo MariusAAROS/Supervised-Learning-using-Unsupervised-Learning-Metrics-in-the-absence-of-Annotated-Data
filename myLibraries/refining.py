e7d2a33bb9a1bb20da83534bd218e1cc3c4ea462
Marius Ortega <marius.ortega@edu.devinci.fr>
2023-05-10 16:33:57 +0200

    Refiner Tester: working

--- myLibraries/refining.py
+++ myLibraries/refining.py
@@ -12,25 +12,28 @@ def updateFileCount(path):
 
     :output updated (int): New number of output file created after the current experimentation.
     """
-    with open(path, "r") as f:
-        value = int(f.read())
-    updated = value + 1
+    if os.path.exists(path):
+        with open(path, "r") as f:
+            value = int(f.read())
+        updated = value + 1
+    else:
+        updated = 1
     with open(path, "w") as f:
-        f.write(str(updated))
+        f.write(str(updated))    
     return updated
 
 #load dataset
 billsum_path = r"C:\Pro\Stages\A4 - DVRC\Work\Datasets\billsum"
 billsum_test = pd.read_json(path_or_buf = billsum_path + r"\us_test_data_final_OFFICIAL.jsonl", lines=True)
 billsum_test = billsum_test.loc[:, ["text", "summary"]]
-size = 50
+size = 2
 subset = billsum_test.iloc[:size, :]
 
 #refine
 w2v = serialized_to_model(r'C:\Pro\Stages\A4 - DVRC\Work\Models\serialized_w2v.pkl')
 r = Refiner(subset["text"], w2v, score, ratio=np.linspace(1, 3, 2), maxSpacing=15, printRange=range(0, 3))
 r.refine()
-res = r.assess()
+assessement = r.assess()
 
 #write output
 main_folder_path = r"C:\Pro\Stages\A4 - DVRC\Work\Supervised-Learning-using-Unsupervised-Learning-Metrics-in-the-absence-of-Annotated-Data\myLibraries\refining_output"
@@ -40,3 +43,10 @@ count = updateFileCount(os.path.join(main_folder_path, countfile_name))
 current_path = os.path.join(main_folder_path, f"experimentation_{count}")
 os.mkdir(current_path)
 
+mainDf = r.to_dataframe()
+scoreDf = assessement["scores"]
+corDf = assessement["correlations"]
+
+mainDf.to_csv(os.path.join(current_path, "main.csv"))
+scoreDf.to_csv(os.path.join(current_path, "scores.csv"))
+corDf.to_csv(os.path.join(current_path, "correlations.csv"))
