329765576ce514e71fbad6e8f7aa77754c730621
Marius Ortega <marius.ortega@edu.devinci.fr>
2023-05-10 17:06:58 +0200

    Refiner Tester: Runtime + small changes

--- myLibraries/refining.py
+++ myLibraries/refining.py
@@ -4,6 +4,7 @@ import numpy as np
 from custom_score.utils import serialized_to_model
 from custom_score.score import score
 import os
+from datetime import datetime
 
 #utils
 def updateFileCount(path):
@@ -30,10 +31,15 @@ size = 2
 subset = billsum_test.iloc[:size, :]
 
 #refine
+start = datetime.now()
 w2v = serialized_to_model(r'C:\Pro\Stages\A4 - DVRC\Work\Models\serialized_w2v.pkl')
 r = Refiner(subset["text"], w2v, score, ratio=np.linspace(1, 3, 2), maxSpacing=15, printRange=range(0, 3))
 r.refine()
 assessement = r.assess()
+stop = datetime.now()
+
+#runtime
+runtime = (stop - start)
 
 #write output
 main_folder_path = r"C:\Pro\Stages\A4 - DVRC\Work\Supervised-Learning-using-Unsupervised-Learning-Metrics-in-the-absence-of-Annotated-Data\myLibraries\refining_output"
@@ -43,10 +49,12 @@ count = updateFileCount(os.path.join(main_folder_path, countfile_name))
 current_path = os.path.join(main_folder_path, f"experimentation_{count}")
 os.mkdir(current_path)
 
-mainDf = r.to_dataframe()
+#mainDf = r.to_dataframe()
 scoreDf = assessement["scores"]
 corDf = assessement["correlations"]
 
-mainDf.to_csv(os.path.join(current_path, "main.csv"))
+#mainDf.to_csv(os.path.join(current_path, "main.csv"))
 scoreDf.to_csv(os.path.join(current_path, "scores.csv"))
 corDf.to_csv(os.path.join(current_path, "correlations.csv"))
+with open(os.path.join(current_path, "runtimes.txt"), "w") as f:
+    f.write(str(runtime))
