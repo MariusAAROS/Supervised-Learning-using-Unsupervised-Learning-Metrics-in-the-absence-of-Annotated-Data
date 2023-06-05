8377b7bf5115c64b35250f2d2bde04473124e4e2
Marius Ortega <marius.ortega@edu.devinci.fr>
2023-06-05 12:23:26 +0200

    MARScore: ILP Functionnal

---
+++ myLibraries/MARScore_output/ilp_out.sol
@@ -0,0 +1,154 @@
+Problem:    
+Rows:       39
+Columns:    91 (91 integer, 91 binary)
+Non-zeros:  451
+Status:     INTEGER OPTIMAL
+Objective:  score = 16484 (MAXimum)
+
+   No.   Row name        Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 index_0                     6             0               
+     2 index_1                     0             0               
+     3 index_2                     1             0               
+     4 index_3                     0             0               
+     5 index_4                     2             0               
+     6 index_5                     1             0               
+     7 index_6                     2             0               
+     8 index_7                     0             0               
+     9 index_8                     3             0               
+    10 index_9                     3             0               
+    11 index_10                    5             0               
+    12 index_11                    1             0               
+    13 index_12                    1             0               
+    14 index_13                    1             0               
+    15 index_14                    1             0               
+    16 index_15                    1             0               
+    17 index_16                    1             0               
+    18 index_17                    0             0               
+    19 index_18                    1             0               
+    20 index_19                    0             0               
+    21 index_20                    2             0               
+    22 index_21                    1             0               
+    23 index_22                    1             0               
+    24 index_23                    1             0               
+    25 index_24                    4             0               
+    26 index_25                    0             0               
+    27 index_26                    2             0               
+    28 index_27                    2             0               
+    29 index_28                    1             0               
+    30 index_29                    1             0               
+    31 index_30                    5             0               
+    32 index_31                    1             0               
+    33 index_32                    0             0               
+    34 index_33                    1             0               
+    35 index_34                    3             0               
+    36 index_35                    2             0               
+    37 index_36                    2             0               
+    38 index_37                    2             0               
+    39 length                      9                        2000 
+
+   No. Column name       Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 c0           *              1             0             1 
+     2 c1           *              1             0             1 
+     3 c2           *              1             0             1 
+     4 c3           *              1             0             1 
+     5 c4           *              1             0             1 
+     6 c5           *              1             0             1 
+     7 c6           *              1             0             1 
+     8 c7           *              1             0             1 
+     9 c8           *              1             0             1 
+    10 c9           *              1             0             1 
+    11 c10          *              1             0             1 
+    12 c11          *              1             0             1 
+    13 c12          *              1             0             1 
+    14 c13          *              1             0             1 
+    15 c14          *              1             0             1 
+    16 c15          *              1             0             1 
+    17 c16          *              1             0             1 
+    18 c17          *              1             0             1 
+    19 c18          *              1             0             1 
+    20 c19          *              1             0             1 
+    21 c20          *              1             0             1 
+    22 c21          *              1             0             1 
+    23 c22          *              1             0             1 
+    24 c23          *              1             0             1 
+    25 c24          *              1             0             1 
+    26 c25          *              1             0             1 
+    27 c26          *              1             0             1 
+    28 c27          *              1             0             1 
+    29 c28          *              1             0             1 
+    30 c29          *              1             0             1 
+    31 c30          *              1             0             1 
+    32 c31          *              1             0             1 
+    33 c32          *              1             0             1 
+    34 c33          *              1             0             1 
+    35 c34          *              1             0             1 
+    36 c35          *              1             0             1 
+    37 c36          *              1             0             1 
+    38 c37          *              1             0             1 
+    39 s0           *              1             0             1 
+    40 s1           *              0             0             1 
+    41 s2           *              1             0             1 
+    42 s3           *              0             0             1 
+    43 s4           *              0             0             1 
+    44 s5           *              0             0             1 
+    45 s6           *              1             0             1 
+    46 s7           *              0             0             1 
+    47 s8           *              0             0             1 
+    48 s9           *              0             0             1 
+    49 s10          *              0             0             1 
+    50 s11          *              0             0             1 
+    51 s12          *              0             0             1 
+    52 s13          *              0             0             1 
+    53 s15          *              0             0             1 
+    54 s16          *              0             0             1 
+    55 s18          *              1             0             1 
+    56 s19          *              0             0             1 
+    57 s20          *              0             0             1 
+    58 s21          *              1             0             1 
+    59 s22          *              0             0             1 
+    60 s23          *              0             0             1 
+    61 s25          *              0             0             1 
+    62 s26          *              0             0             1 
+    63 s27          *              0             0             1 
+    64 s29          *              0             0             1 
+    65 s30          *              0             0             1 
+    66 s31          *              0             0             1 
+    67 s33          *              0             0             1 
+    68 s34          *              0             0             1 
+    69 s36          *              0             0             1 
+    70 s37          *              0             0             1 
+    71 s38          *              1             0             1 
+    72 s39          *              0             0             1 
+    73 s40          *              1             0             1 
+    74 s41          *              0             0             1 
+    75 s42          *              0             0             1 
+    76 s43          *              0             0             1 
+    77 s44          *              0             0             1 
+    78 s45          *              0             0             1 
+    79 s46          *              0             0             1 
+    80 s47          *              0             0             1 
+    81 s48          *              0             0             1 
+    82 s49          *              0             0             1 
+    83 s50          *              0             0             1 
+    84 s51          *              0             0             1 
+    85 s52          *              0             0             1 
+    86 s14          *              0             0             1 
+    87 s17          *              0             0             1 
+    88 s24          *              0             0             1 
+    89 s32          *              1             0             1 
+    90 s35          *              1             0             1 
+    91 s28          *              0             0             1 
+
+Integer feasibility conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
