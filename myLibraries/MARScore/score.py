c8f3725c1e239c892a4ed015cc717b23737ae33b
Marius Ortega <ortegasaurelmarius@gmail.com>
2023-06-07 14:24:56 +0200

    MARScore: Assess function

--- myLibraries/MARScore/score.py
+++ myLibraries/MARScore/score.py
@@ -1,9 +1,21 @@
 from MARScore.utils import *
-import hdbscan
 import os
 
+import sys
+sys.path.append(get_git_root())
+sys.path.append(os.path.join(get_git_root(), "myLibraries"))
+
+from custom_score.score import score
+from custom_score.utils import model_load
+from rouge_score import rouge_scorer
+import bert_score
+import hdbscan
+import pandas as pd
+from scipy.stats import pearsonr
+from BARTScore.bart_score import BARTScorer
+
 class MARSCore():
-    def __init__(self, corpus, model=BertModel.from_pretrained('bert-base-uncased', 
+    def __init__(self, corpus, gold, model=BertModel.from_pretrained('bert-base-uncased', 
                                                                output_hidden_states=True), 
                                tokenizer=BertTokenizer.from_pretrained('bert-base-uncased')) -> None:
         """
@@ -14,7 +26,8 @@ class MARSCore():
         :param3 tokenizer (transformer) Transformer used to create token from a plain text. 
         """
         self.corpus = corpus
-        self.summary = []
+        self.gold = gold
+        self.summaries = []
         self.model = model
         self.tokenizer = tokenizer
     
@@ -41,11 +54,98 @@ class MARSCore():
             os.system(f'glpsol --tmlim 100 --lp "{os.path.join(dirpath, "ilp_in.ilp")}" -o "{os.path.join(dirpath, "ilp_out.sol")}"')
             selected = readILP()
 
-            #summary construction
+            #summaries construction
             sentences = indiv.split(".")
             sentences.pop()
             sum_sentences = []
             for i, value in enumerate(selected):
                 if value == 1:
                     sum_sentences.append(sentences[i]+".")
-            self.summary.append(" ".join(sum_sentences))
+            self.summaries.append(" ".join(sum_sentences))
+
+    def assess(self, start=0, stop=None, verbose=True):
+        """
+        Assesses quality of the refined corpus by computing Static BERTscore and Rouge-Score on the refined version compared to it's initial version.
+
+        :param1 self (Refiner): Refiner Object (see __init__ function for more details).
+        :param2 start (int): Starting index to assess.
+        :param3 stop (int): Ending index to assess.
+        :param4 verbose (Boolean): When put to True, assess results will be printed.
+
+        :output (dict): Dictionnary containing both the scores of Static BERTScore, BERTScore, BARTScore and Rouge as well as their correlation.
+        """
+        assert self.summaries != None, "refined corpus doesn't exists"
+        
+        if stop == None:
+            stop = len(self.summaries)
+        subset_summaries = self.summaries[start:stop]
+        subset_gold = self.gold[start:stop]
+
+        #Static BERTScore computation
+        w2v = model_load("Word2Vec", True)
+        customScore = score(w2v, subset_summaries, subset_gold)
+        #customScore = [parseScore(curScore) for curScore in scoreOut]
+
+        #Rouge-Score computation
+        rougeScorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)
+        rougeScore = [rougeScorer.score(c, r) for c, r in zip(subset_gold, subset_summaries)]
+
+        #BERTScore computation
+        with nostd():
+            bertscore = bert_score.score(subset_summaries, subset_gold, lang="en", verbose=0)
+        #bartscore
+        bart_scorer = BARTScorer(device='cuda:0', checkpoint='facebook/bart-large-cnn')
+        bartscore = bart_scorer.score(subset_summaries, subset_gold, batch_size=4)
+
+        #Data formating
+        custom_R = [round(t[0], 2) for t in customScore]
+        custom_P = [round(t[1], 2) for t in customScore]
+        custom_F = [round(t[2], 2) for t in customScore]
+        bertscore_R = [round(t.item(), 2) for t in bertscore[1]]
+        bertscore_P = [round(t.item(), 2) for t in bertscore[0]]
+        bertscore_F = [round(t.item(), 2) for t in bertscore[2]]
+        bartscore = [round(t, 2) for t in bartscore]
+        rouge1_R = [round(t['rouge1'][0], 2) for t in rougeScore]
+        rouge2_R = [round(t['rouge2'][0], 2) for t in rougeScore]
+        rougeL_R = [round(t['rougeL'][0], 2) for t in rougeScore]
+
+        dfCustom = pd.DataFrame({'CBERT-R' : custom_R,
+                                 'CBERT-P' : custom_P,
+                                 'CBERT-F' : custom_F,
+                                 'BERTScore-R' : bertscore_R,
+                                 'BERTScore-P' : bertscore_P,
+                                 'BERTScore-F' : bertscore_F,
+                                 'BARTScore' : bartscore,
+                                 'R-1' : rouge1_R,
+                                 'R-2' : rouge2_R,
+                                 'R-L' : rougeL_R
+                                })
+
+        #Correlation estimation
+        pearsonCor_c_r1 = np.round(pearsonr(custom_F, rouge1_R), 2)
+        pearsonCor_c_r2 = np.round(pearsonr(custom_F, rouge2_R), 2)
+        pearsonCor_c_rl = np.round(pearsonr(custom_F, rougeL_R), 2)
+        pearsonCor_bertscore_r1 = np.round(pearsonr(bertscore_F, rouge1_R), 2)
+        pearsonCor_bertscore_r2 = np.round(pearsonr(bertscore_F, rouge2_R), 2)
+        pearsonCor_bertscore_rl = np.round(pearsonr(bertscore_F, rougeL_R), 2)
+        pearsonCor_bartscore_r1 = np.round(pearsonr(bartscore, rouge1_R), 2)
+        pearsonCor_bartscore_r2 = np.round(pearsonr(bartscore, rouge2_R), 2)
+        pearsonCor_bartscore_rl = np.round(pearsonr(bartscore, rougeL_R), 2)
+
+        dfCor = pd.DataFrame({'pearson_CBERT_R-1' : pearsonCor_c_r1,
+                              'pearson_CBERT_R-2' : pearsonCor_c_r2,
+                              'pearson_CBERT_R-L' : pearsonCor_c_rl,
+                              'pearson_BERT_R-1' : pearsonCor_bertscore_r1,
+                              'pearson_BERT_R-2' : pearsonCor_bertscore_r2,
+                              'pearson_BERT_R-l' : pearsonCor_bertscore_rl,
+                              'pearson_BART_R-1' : pearsonCor_bartscore_r1,
+                              'pearson_BART_R-2' : pearsonCor_bartscore_r2,
+                              'pearson_BART_R-l' : pearsonCor_bartscore_rl}, index=["Pearson score", "p-value"])
+        if verbose:
+            printout = "Scores: \n"
+            printout += dfCustom.to_string() + "\n\n"
+            printout += "Correlations: \n"
+            printout += dfCor.to_string()
+            print(printout)
+
+        return {"scores": dfCustom, "correlations": dfCor}
--- myLibraries/MARScore/utils.py
+++ myLibraries/MARScore/utils.py
@@ -8,6 +8,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from custom_score.utils import cleanString, get_git_root
 import os
 import re
+import io
+import sys
+import git
+from contextlib import contextmanager
 
 
 def tokenizeCorpus(corpus, model=BertModel.from_pretrained('bert-base-uncased', 
@@ -409,3 +413,26 @@ def readILP(rel_path="myLibraries\MARScore_output\ilp_out.sol"):
     matches = re.findall(pattern, raw)
     result = [int(match) for match in matches]
     return result
+
+def get_git_root():
+    """
+    Return the path to the current git directory's root.
+
+    output git_root (str): Path of the current git directory's root.
+    """
+    git_repo = git.Repo(os.path.abspath(__file__), search_parent_directories=True)
+    git_root = git_repo.git.rev_parse("--show-toplevel")
+    return git_root
+
+@contextmanager
+def nostd():
+    """
+    Decorator supressing console output. Source : https://stackoverflow.com/questions/2828953/silence-the-stdout-of-a-function-in-python-without-trashing-sys-stdout-and-resto
+    """
+    save_stdout = sys.stdout
+    save_stderr = sys.stderr
+    sys.stdout = io.BytesIO()
+    sys.stderr = io.BytesIO()
+    yield
+    sys.stdout = save_stdout
+    sys.stderr = save_stderr
